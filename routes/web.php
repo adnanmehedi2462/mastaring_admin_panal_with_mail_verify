6f004700d4a0dccaf5647f7e5b33cce38b723d82
adnan <adnanjohan54@gmail.com>
2019-10-21 21:29:21 +0600

    smart code deya first upload ata

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+
+
+
+Route::group(['middleware'=>'verified'],function(){
+
+
+
+Route::get('inbox', function () {
+   echo "inbox";
+})->name('inbox');
+
+
+});
